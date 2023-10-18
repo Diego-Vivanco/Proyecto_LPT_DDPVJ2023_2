197b6c12a2ed0eb6af862781fa84a71deab82d36
Diego-Vivanco <60807454+Diego-Vivanco@users.noreply.github.com>
2023-10-18 00:41:59 -0600

    Guardado de Items - P2
    Se creó un nuevo script para la detección de los items de acuerdo a sus funciones.

--- LaPrisionDelTartaro/Assets/Scripts/LogicBox.cs
+++ LaPrisionDelTartaro/Assets/Scripts/LogicBox.cs
@@ -13,6 +13,40 @@ public class LogicBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                if(hitInfo.collider.gameObject.tag == "objeto" && hitInfo.collider.gameObject.GetComponent<LogicItems>().destuirConCursor == true)
+                {
+                    hitInfo.collider.gameObject.GetComponent<LogicItems>().Efecto();
+                    Destroy(hitInfo.collider.gameObject);
+                }
+            }
+        }
     }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag =="objeto" && other.GetComponent<LogicItems>().destruirAutomatico == true)
+        {
+            other.GetComponent<LogicItems>().Efecto();
+            Destroy(other.gameObject);
+        }
+
+        if (other.tag == "objeto")
+        {
+            if(Input.GetMouseButtonDown(1) && other.GetComponent<LogicItems>().destruirAutomatico == false)
+            {
+                other.GetComponent <LogicItems>().Efecto();
+                Destroy(other.gameObject);
+
+            }
+        }
+    }
+
+
 }
