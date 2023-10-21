65ec528a6e6ac6ee4bea088323d2b1e89cdef51e
Diego-Vivanco <60807454+Diego-Vivanco@users.noreply.github.com>
2023-10-21 00:11:37 -0600

    Creación de un fuego con Partículas
    Se creo el efecto de fuego con el sistema de particulas.

---
+++ LaPrisionDelTartaro/Assets/Scripts/SoundSFxPegaso.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSFxPegaso : MonoBehaviour
+{
+    public AudioSource SFxPegasoSource;
+
+    public AudioClip[] SFxClip;
+
+    public static SoundMusicManager InstanceSFxPegaso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
