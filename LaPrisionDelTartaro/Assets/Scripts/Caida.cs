9a4267994ef21ab4ceb6f146a4550a9b1c463b91
Diego-Vivanco <60807454+Diego-Vivanco@users.noreply.github.com>
2023-10-28 22:55:02 -0600

    Ajustes escenario Nivel 3
    Se hicieron ajustes al modelo del templo del nivel 3.

--- LaPrisionDelTartaro/Assets/Scripts/Caida.cs
+++ LaPrisionDelTartaro/Assets/Scripts/Caida.cs
@@ -6,21 +6,43 @@ using UnityEngine.SceneManagement;
 public class Caida : MonoBehaviour
 {
     public int lifes = 3;
+    public int cont = 0;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "vacio" && lifes == 0)
+        if(other.tag == "vacio")
         {
-            Debug.Log("JUEGO PERDIDO");
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            //lifes -= 1;
+            while (lifes > 0)
+            {
+                lifes -= 1;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            }
+
+            Debug.Log("PERDISTE JUEGO");
+
         }
-        if(other.tag == "vacio" && lifes  > 0)
-        {
-            lifes-=1; ;
-            Debug.Log("lifes: " + lifes);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+
+
+        //if(other.tag == "vacio")
+        //{
+        //    cont +=1;
+        //    Debug.Log("Contador: "+ cont);
+        //    if(cont == 3)
+        //    {
+        //        Debug.Log("JUEGO PERDIDO");
+        //    }
+        //    else
+        //    {
+        //        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //        lifes -= 1;
+        //        Debug.Log("lifes: " + lifes);
+        //    }
+        //    //Debug.Log("JUEGO PERDIDO");
+        //    //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //    //lifes -= 1;
+        //}
+
 
     }
 }
