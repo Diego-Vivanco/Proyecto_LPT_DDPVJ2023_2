c790a58ed8cfc53435b98bc7791811a10200c288
Diego-Vivanco <60807454+Diego-Vivanco@users.noreply.github.com>
2023-10-30 23:01:14 -0600

    Script controladoe Muerto 1 (Continuación)
    Se continua implementando el comportamiento del enemigo muerto.

--- LaPrisionDelTartaro/Assets/Scripts/DeadController.cs
+++ LaPrisionDelTartaro/Assets/Scripts/DeadController.cs
@@ -10,37 +10,63 @@ public class DeadController : MonoBehaviour
     public Animator animator;
     public Quaternion angulo;
     public float grado;
+
+    public GameObject target;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        target = GameObject.Find("Pegaso");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Comportamiento();
     }
 
     public void Comportamiento()
     {
-        cronometro += 1 * Time.deltaTime;
-        if(cronometro >= 4)
+        if(Vector3.Distance(transform.position, target.transform.position) > 5)
         {
-            rutina = Random.Range(0, 2);
-            cronometro = 0;
+            animator.SetBool("walk", false);
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                cronometro = 0;
+            }
+
+            switch (rutina)
+            {
+                case 0:
+                    animator.SetBool("walk", false);
+
+                    break;
+
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    animator.SetBool("walk", true);
+                    break;
+            }
+        }
+        else
+        {
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
+            animator.SetBool("walk", false);
+            animator.SetBool("run", true);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
         }
 
-        switch (rutina)
-        {
-            case 0:
-                animator.SetBool("walk", false);
-
-                break;
-
-            case 1:
-                grado = Random.Range(0, 360);
-                break;
-        }   
     }
 }
